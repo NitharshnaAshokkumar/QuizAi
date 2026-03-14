008d29392ebf309ab02da4634875c4c2305ef036
Nitharshna Ashokkumar <nitharshna17@gmail.com>
2026-03-14 16:08:05 +0530

    Fix: Add STATIC_ROOT for deployment

--- backend/quiz_project/settings.py
+++ backend/quiz_project/settings.py
@@ -97,6 +97,7 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
